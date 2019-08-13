e0bb744c93fd80459c589e80d0382f30f56662d1
Tucker Burgin <tburgin@umich.edu>
2019-08-13 13:55:42 -0400

    YAPF'd the code base

--- molssiexample/tests/test_mymath.py
+++ molssiexample/tests/test_mymath.py
@@ -2,14 +2,17 @@ import molssiexample as me
 import random
 import pytest
 
-@pytest.mark.parametrize('x, answer',[(0,1), (1,2), (2,2.5), (10, 2.7182)])
+
+@pytest.mark.parametrize('x, answer', [(0, 1), (1, 2), (2, 2.5), (10, 2.7182)])
 def test_euler(x, answer):
-	assert me.mymath.euler(x) == pytest.approx(answer, abs=0.01)
+    assert me.mymath.euler(x) == pytest.approx(answer, abs=0.01)
+
 
 def test_euler_fail():
-	with pytest.raises(ValueError):
-		me.mymath.euler(-1)
+    with pytest.raises(ValueError):
+        me.mymath.euler(-1)
+
 
 def test_pi():
-	random.seed(0)
-	assert me.mymath.pi(10000) == pytest.approx(3.14159, abs=0.01)
+    random.seed(0)
+    assert me.mymath.pi(10000) == pytest.approx(3.14159, abs=0.01)
